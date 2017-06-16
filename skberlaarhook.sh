49b386417e325d2663a79e9eb8d12fe1eab47182
Sven De Gronckel <Sven@Svens-MacBook-Pro.local>
2017-06-16 16:18:41 +0200

    10th commit

---
+++ skberlaarhook.sh
@@ -0,0 +1,4 @@
+date >> /app/nodeprojects/github/logs/skberlaarhook.log 2>&1
+cd /app/nodeprojects/github/skberlaar
+git pull https://github.com/Hoegie/skberlaar.git >> /app/nodeprojects/github/logs/skberlaarhook.log 2>&1
+
